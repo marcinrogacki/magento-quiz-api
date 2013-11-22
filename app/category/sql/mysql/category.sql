4784a1d3a9b8b6075055f245c2048b94965954a5
Marcin Rogacki <mrogacki@nexway.com>
2013-11-22 16:49:11 +0100

    category and subcategory added

--- app/category/sql/mysql/category.sql
+++ app/category/sql/mysql/category.sql
@@ -5,20 +5,20 @@ CREATE TABLE category (
   CONSTRAINT FK_CATEGORY_CATEGORY FOREIGN KEY (parent_id) REFERENCES category (id) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO category VALUES(1, 'Back-end', null);
-INSERT INTO category VALUES(2, 'Front-end', null);
+INSERT INTO category VALUES(1, null, 'Back-end');
+INSERT INTO category VALUES(2, null, 'Front-end');
 
 --  BACKEND --
-INSERT INTO category VALUES(3, 'Basics', 1);
-INSERT INTO category VALUES(4, 'Request flow', 1);
-INSERT INTO category VALUES(5, 'Rendering', 1);
-INSERT INTO category VALUES(6, 'Working with Database in Magento', 1);
-INSERT INTO category VALUES(7, 'EAV', 1);
-INSERT INTO category VALUES(8, 'Adminhtml', 1);
-INSERT INTO category VALUES(9, 'Catalog', 1)t
-INSERT INTO category VALUES(10, 'Checkout', 1);
-INSERT INTO category VALUES(11, 'Sales and Customers', 1);
-INSERT INTO category VALUES(12, 'Advanced features', 1);
-INSERT INTO category VALUES(13, 'Enterprise edition', 1);
+INSERT INTO category VALUES(3, 1, 'Basics');
+INSERT INTO category VALUES(4, 1, 'Request flow');
+INSERT INTO category VALUES(5, 1, 'Rendering');
+INSERT INTO category VALUES(6, 1, 'Working with Database in Magento');
+INSERT INTO category VALUES(7, 1, 'EAV');
+INSERT INTO category VALUES(8, 1, 'Adminhtml');
+INSERT INTO category VALUES(9, 1, 'Catalog');
+INSERT INTO category VALUES(10, 1, 'Checkout');
+INSERT INTO category VALUES(11, 1, 'Sales and Customers');
+INSERT INTO category VALUES(12, 1, 'Advanced features');
+INSERT INTO category VALUES(13, 1, 'Enterprise edition');
 
 --  FRONTEND --

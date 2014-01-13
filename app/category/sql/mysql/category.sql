67ab17fe6f672845d40d049ed583b31ebcaaabcf
Marcin Rogacki <mrogacki@nexway.com>
2014-01-13 10:59:24 +0100

    front-end categories

--- app/category/sql/mysql/category.sql
+++ app/category/sql/mysql/category.sql
@@ -20,3 +20,13 @@ INSERT INTO category VALUES(11, 1, 'Sales and Customers');
 INSERT INTO category VALUES(12, 1, 'Advanced features');
 INSERT INTO category VALUES(13, 1, 'Enterprise edition');
 
+INSERT INTO category VALUES(14, 2, 'Use the Magento Design Fallback System');
+INSERT INTO category VALUES(15, 2, 'Use Layout XML to Customize a Theme');
+INSERT INTO category VALUES(16, 2, 'Create and Customize Template Files');
+INSERT INTO category VALUES(17, 2, 'Effectively Use the Magento Block-Template Design System');
+INSERT INTO category VALUES(18, 2, 'Identify Where to Locate Files and Create New Files in the Theming-related Directory Structure');
+INSERT INTO category VALUES(19, 2, 'Customize and Create JavaScript Within the Magento Framework');
+INSERT INTO category VALUES(20, 2, 'Use CSS Effectively to Customize Magento Look and Feel');
+INSERT INTO category VALUES(21, 2, 'Customize the Look and Feel of Specific Magento Pages');
+INSERT INTO category VALUES(22, 2, 'Correctly Use the Admin Configuration Scopes');
+INSERT INTO category VALUES(23, 2, 'Implement Internationalization of Frontend Pages');
